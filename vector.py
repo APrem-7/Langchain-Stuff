0d47fc34a60c2e2099dcb49751c7f73a72b18092
APrem-7 <ap7leo@gmail.com>
2025-11-29 05:37:22 +0530

    refactor vector.py to enhance document processing logic i.e the embeddings

--- vector.py
+++ vector.py
@@ -5,3 +5,20 @@ import os
 import pandas as pd
 
 df = pd.read_csv("realistic_restaurant_reviews.csv")
+
+db_location = "./chrome_langchain_db"
+add_documents = not os.path.exists(db_location)
+
+if add_documents:
+    documents = []
+    ids= []
+
+    for i,row in df.iterrows():
+        document =Document(
+            page_content = row["Title"]+" "+row["Review"]
+            metadata={"rating":row["rating"],"date":row["Date"]},
+            id=str(i)
+        )
+        ids.append(str(i))
+        documents.append(document)
+    
